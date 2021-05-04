539e9dd5c57e70ca88eb8fb2bab0512243442247
Burak Biçkioğlu <burakbickioglu@gmail.com>
2021-05-04 14:58:20 +0300

    backend completed

--- WindowsFormsApp1/frmBakiyeEkle.Designer.cs
+++ WindowsFormsApp1/frmBakiyeEkle.Designer.cs
@@ -32,83 +32,41 @@ namespace WindowsFormsApp1
             this.btnIstekGonder = new System.Windows.Forms.Button();
             this.txtTutar = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnIstekGonder
             // 
-            this.btnIstekGonder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(99)))), ((int)(((byte)(155)))));
-            this.btnIstekGonder.FlatAppearance.BorderSize = 0;
-            this.btnIstekGonder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIstekGonder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnIstekGonder.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnIstekGonder.Location = new System.Drawing.Point(185, 285);
+            this.btnIstekGonder.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnIstekGonder.Location = new System.Drawing.Point(88, 59);
             this.btnIstekGonder.Name = "btnIstekGonder";
-            this.btnIstekGonder.Size = new System.Drawing.Size(121, 31);
+            this.btnIstekGonder.Size = new System.Drawing.Size(121, 35);
             this.btnIstekGonder.TabIndex = 7;
             this.btnIstekGonder.Text = "İstek Gönder";
-            this.btnIstekGonder.UseVisualStyleBackColor = false;
+            this.btnIstekGonder.UseVisualStyleBackColor = true;
             this.btnIstekGonder.Click += new System.EventHandler(this.btnIstekGonder_Click);
             // 
             // txtTutar
             // 
             this.txtTutar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtTutar.Location = new System.Drawing.Point(185, 232);
+            this.txtTutar.Location = new System.Drawing.Point(88, 12);
             this.txtTutar.Name = "txtTutar";
-            this.txtTutar.Size = new System.Drawing.Size(158, 32);
+            this.txtTutar.Size = new System.Drawing.Size(121, 27);
             this.txtTutar.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(134)))), ((int)(((byte)(48)))));
-            this.label2.Location = new System.Drawing.Point(185, 190);
+            this.label2.Location = new System.Drawing.Point(22, 11);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 28);
+            this.label2.Size = new System.Drawing.Size(60, 25);
             this.label2.TabIndex = 5;
             this.label2.Text = "Tutar:";
             // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(190)))), ((int)(((byte)(166)))));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(1, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(607, 155);
-            this.panel1.TabIndex = 8;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.label1.Location = new System.Drawing.Point(242, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(199, 46);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Bakiye Ekle";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.add_wallet_1853738_15726282;
-            this.pictureBox1.Location = new System.Drawing.Point(126, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(110, 110);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
-            // 
             // frmBakiyeEkle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 362);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(226, 108);
             this.Controls.Add(this.btnIstekGonder);
             this.Controls.Add(this.txtTutar);
             this.Controls.Add(this.label2);
@@ -116,9 +74,6 @@ namespace WindowsFormsApp1
             this.Name = "frmBakiyeEkle";
             this.Text = "Bakiye Ekle";
             this.Load += new System.EventHandler(this.frmBakiyeEkle_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,8 +84,5 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Button btnIstekGonder;
         private System.Windows.Forms.TextBox txtTutar;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsApp1/frmKullaniciGiris.Designer.cs
+++ WindowsFormsApp1/frmKullaniciGiris.Designer.cs
@@ -34,26 +34,19 @@ namespace WindowsFormsApp1
             this.label2 = new System.Windows.Forms.Label();
             this.txtKullaniciSifre = new System.Windows.Forms.TextBox();
             this.btnGiris = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtKullaniciAd
             // 
-            this.txtKullaniciAd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtKullaniciAd.Location = new System.Drawing.Point(173, 273);
+            this.txtKullaniciAd.Location = new System.Drawing.Point(134, 12);
             this.txtKullaniciAd.Name = "txtKullaniciAd";
-            this.txtKullaniciAd.Size = new System.Drawing.Size(166, 34);
+            this.txtKullaniciAd.Size = new System.Drawing.Size(166, 33);
             this.txtKullaniciAd.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(134)))), ((int)(((byte)(48)))));
-            this.label1.Location = new System.Drawing.Point(173, 245);
+            this.label1.Location = new System.Drawing.Point(9, 15);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 25);
             this.label1.TabIndex = 1;
@@ -62,9 +55,7 @@ namespace WindowsFormsApp1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(134)))), ((int)(((byte)(48)))));
-            this.label2.Location = new System.Drawing.Point(173, 324);
+            this.label2.Location = new System.Drawing.Point(74, 61);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 25);
             this.label2.TabIndex = 3;
@@ -72,53 +63,27 @@ namespace WindowsFormsApp1
             // 
             // txtKullaniciSifre
             // 
-            this.txtKullaniciSifre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtKullaniciSifre.Location = new System.Drawing.Point(173, 352);
+            this.txtKullaniciSifre.Location = new System.Drawing.Point(134, 58);
             this.txtKullaniciSifre.Name = "txtKullaniciSifre";
-            this.txtKullaniciSifre.Size = new System.Drawing.Size(166, 34);
+            this.txtKullaniciSifre.Size = new System.Drawing.Size(166, 33);
             this.txtKullaniciSifre.TabIndex = 2;
             this.txtKullaniciSifre.UseSystemPasswordChar = true;
             // 
             // btnGiris
             // 
-            this.btnGiris.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(99)))), ((int)(((byte)(155)))));
-            this.btnGiris.FlatAppearance.BorderSize = 0;
-            this.btnGiris.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnGiris.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnGiris.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnGiris.Location = new System.Drawing.Point(226, 408);
+            this.btnGiris.Location = new System.Drawing.Point(161, 106);
             this.btnGiris.Name = "btnGiris";
-            this.btnGiris.Size = new System.Drawing.Size(113, 35);
+            this.btnGiris.Size = new System.Drawing.Size(113, 33);
             this.btnGiris.TabIndex = 4;
-            this.btnGiris.Text = "Giriş Yap";
-            this.btnGiris.UseVisualStyleBackColor = false;
+            this.btnGiris.Text = "Giriş";
+            this.btnGiris.UseVisualStyleBackColor = true;
             this.btnGiris.Click += new System.EventHandler(this.btnGiris_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.userl;
-            this.pictureBox1.Location = new System.Drawing.Point(207, 25);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(160, 160);
-            this.pictureBox1.TabIndex = 5;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(190)))), ((int)(((byte)(166)))));
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(0, 1);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(576, 204);
-            this.panel1.TabIndex = 6;
             // 
             // frmKullaniciGiris
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(576, 489);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(319, 153);
             this.Controls.Add(this.btnGiris);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtKullaniciSifre);
@@ -130,9 +95,8 @@ namespace WindowsFormsApp1
             this.Name = "frmKullaniciGiris";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Kullanıcı Giriş";
+            //this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmKullaniciGiris_FormClosed);
             this.Load += new System.EventHandler(this.frmKullaniciGiris_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -145,7 +109,5 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtKullaniciSifre;
         private System.Windows.Forms.Button btnGiris;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
     }
 }
